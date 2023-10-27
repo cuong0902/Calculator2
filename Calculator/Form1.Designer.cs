ddbd8174d94ce44352c1aaf616babc9375fcdf6b
cuong0902 <2051052015cuong@ou.edu.vn>
2023-10-27 11:31:43 +0700

    xu ly công

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -88,12 +88,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(316, 215);
+            this.button1.Location = new System.Drawing.Point(292, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 29);
             this.button1.TabIndex = 6;
             this.button1.Text = "Công";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
